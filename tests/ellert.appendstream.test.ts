7ba41607f1a8a1f6a7a80da95b21f40a769311cc
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2023-10-30 00:17:03 +0100

    refactor(test): use Event<T> for test

--- tests/ellert.appendstream.test.ts
+++ tests/ellert.appendstream.test.ts
@@ -7,6 +7,9 @@ import {
 } from './Constants'
 import { Pool, PoolClient } from 'pg'
 import { pgAdmin } from '../src/pg-admin'
+import { Event } from '../src/Event'
+
+type ProjectKickedOffEvent = Event<`ProjectKickedOffEvent`, { name: string }>
 
 describe('when appending a new to event to a stream', async (): Promise<void> => {
   let db: PoolClient = null
@@ -18,9 +21,14 @@ describe('when appending a new to event to a stream', async (): Promise<void> =>
   const testPoolConfig = getTestDatabasePoolConfig(testDatabase)
   const store = await Ellert({ poolConfig: testPoolConfig }, [])
 
-  const streamResult = await store.appendToStream('test', [
-    { type: 'test', data: {}, metadata: {} }
-  ])
+  const event: ProjectKickedOffEvent = {
+    type: 'ProjectKickedOffEvent',
+    data: {
+      name: 'Ellert'
+    },
+    metadata: {}
+  }
+  const streamResult = await store.appendToStream('test', [event])
 
   const testEvent = await store.getLastEvent('test')
   beforeEach(async () => {
@@ -30,13 +38,13 @@ describe('when appending a new to event to a stream', async (): Promise<void> =>
   it('should return the stream result including the event', async () => {
     expect(streamResult).toBeDefined()
     expect(streamResult.length).toBe(1)
-    expect(streamResult[0].type).toBe('test')
+    expect(streamResult[0].type).toBe('ProjectKickedOffEvent')
   })
 
   it('should persist the event for this stream', async () => {
     expect(testEvent).toBeDefined()
-    expect(testEvent.type).toBe('test')
-    expect(testEvent.data).toEqual({})
+    expect(testEvent.type).toBe('ProjectKickedOffEvent')
+    expect(testEvent.data).toEqual({ name: 'Ellert' })
     expect(testEvent.metadata).toEqual({})
   })
 
